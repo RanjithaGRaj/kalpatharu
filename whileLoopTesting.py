efcd7089792c05a526bbe7592a95d14ce88b7069
RanjithaGRaj <ranjitharajashekarg@gmail.com>
2025-03-19 16:11:49 +0530

    committing in remote repo

---
+++ whileLoopTesting.py
@@ -0,0 +1,26 @@
+
+#Infinte loop (2 lines)
+#while True:
+#   print("Executing the program!!") # infinite loop, ctrl+c to terminate in terminal
+
+counter = 0
+
+while True:
+    print("Hi Goobe!!")
+    counter += 1
+    if counter == 5:
+        break
+
+
+counter1 = 0
+while counter1 < 5 :
+    print ("Hello Goobe mari !!!!")
+    counter1 += 1
+
+## LIST in while
+tech = ['java','aws','python']
+counter3=0
+while counter3 < len(tech) :
+    print("HI Handi")
+    print (tech[counter3])
+    counter3 += 1
